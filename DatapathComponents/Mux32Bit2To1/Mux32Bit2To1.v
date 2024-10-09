a2cb98fdac86cffa5f5235bc548fbbb5d3bc0694
Jelizarraras <jrelizarraras@arizona.edu>
2024-10-09 15:34:35 -0700

    Update Mux32Bit2To1.v

--- DatapathComponents/Mux32Bit2To1/Mux32Bit2To1.v
+++ DatapathComponents/Mux32Bit2To1/Mux32Bit2To1.v
@@ -11,10 +11,15 @@ module Mux32Bit2To1(out, inA, inB, sel);
 
     output reg [31:0] out;
     
-    input [31:0] inA;
-    input [31:0] inB;
-    input sel;
+    input [31:0] inA;  // 32-bit input A
+    input [31:0] inB;  // 32-bit input B
+    input sel;         // 1-bit select signal
 
-    /* Fill in the implementation here ... */ 
+    always @(*) begin
+        if (sel == 1'b0)
+            out = inA;   // Select inA when sel is 0
+        else
+            out = inB;   // Select inB when sel is 1
+    end
 
 endmodule
