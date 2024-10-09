b32c1d40c5f9a7c620ce689fd85aedd05a1fc975
Joseph Sterling Wallace Jr <jwallace0125@arizona.edu>
2024-10-09 14:13:55 -0700

    Update RegisterFile_tb.v

--- DatapathComponents/RegisterFile/RegisterFile_tb.v
+++ DatapathComponents/RegisterFile/RegisterFile_tb.v
@@ -34,39 +34,16 @@ module RegisterFile_tb();
     );
 
     initial begin
-        Clk <= 1'b0;
+        Clk <= 0;
         forever #10 Clk <= ~Clk;
     end
 
     initial begin
         // Initialize inputs
-        ReadRegister1 = 5'd0;
-        ReadRegister2 = 5'd0;
-        WriteRegister = 5'd0;
-        WriteData = 32'd0;
-        RegWrite = 1'b0;
+        ReadRegister1 = 0;
+        ReadRegister2 = 0;
+        WriteRegister = 0;
+        WriteData = 0;
+        RegWrite = 0;
 
-        // Test Case 1: Write arbitrary values into registers 8 through 25
-        #20;
-        for (int i = 8; i <= 25; i = i + 1) begin
-            WriteRegister = i;
-            WriteData = i * 32'h11111111;
-            RegWrite = 1'b1;
-            #20;
-        end
-        RegWrite = 1'b0;
-
-        // Test Case 2: Read values from registers 8 through 25, two by two
-        #20;
-        for (int i = 8; i <= 24; i = i + 2) begin
-            ReadRegister1 = i;
-            ReadRegister2 = i + 1;
-            #20;
-        end
-
-        // Finish simulation
-        #20;
-        $finish;
-    end
-
-endmodule
+        // Test Case 1: Write arbitrary values into registers 8 through
