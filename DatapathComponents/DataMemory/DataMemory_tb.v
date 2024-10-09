7ed1fec4755cb80331ccb4c81ccca6362d15cdee
Joseph Sterling Wallace Jr <jwallace0125@arizona.edu>
2024-10-09 14:13:09 -0700

    Update DataMemory_tb.v

--- DatapathComponents/DataMemory/DataMemory_tb.v
+++ DatapathComponents/DataMemory/DataMemory_tb.v
@@ -26,63 +26,62 @@ module DataMemory_tb();
         .ReadData(ReadData)
     ); 
 
-	initial begin
-		Clk <= 1'b0;
-		forever #10 Clk <= ~Clk;
-	end
+    initial begin
+        Clk <= 0;
+        forever #10 Clk <= ~Clk;
+    end
 
-	initial begin
-	
+    initial begin
+    
     initial begin
         // Initialize inputs
-        Address = 32'd0;
-        WriteData = 32'd0;
-        MemWrite = 1'b0;
-        MemRead = 1'b0;
+        Address = 0;
+        WriteData = 0;
+        MemWrite = 0;
+        MemRead = 0;
         
         // Test Case 1: Write to memory
         #20;
-        Address = 32'd4;
-        WriteData = 32'hDEADBEEF;
-        MemWrite = 1'b1;
-        MemRead = 1'b0;
+        Address = 4;
+        WriteData = 3735928559;
+        MemWrite = 1;
+        MemRead = 0;
         #20;
-        MemWrite = 1'b0;
+        MemWrite = 0;
 
         // Test Case 2: Read from memory
         #20;
-        MemRead = 1'b1;
+        MemRead = 1;
         #20;
-        MemRead = 1'b0;
+        MemRead = 0;
 
         // Test Case 3: Write to another memory location
         #20;
-        Address = 32'd8;
-        WriteData = 32'hCAFEBABE;
-        MemWrite = 1'b1;
-        MemRead = 1'b0;
+        Address = 8;
+        WriteData = 3405691582;
+        MemWrite = 1;
+        MemRead = 0;
         #20;
-        MemWrite = 1'b0;
+        MemWrite = 0;
 
         // Test Case 4: Read from the second memory location
         #20;
-        Address = 32'd8;
-        MemRead = 1'b1;
+        Address = 8;
+        MemRead = 1;
         #20;
-        MemRead = 1'b0;
+        MemRead = 0;
 
         // Test Case 5: Read from the first memory location again
         #20;
-        Address = 32'd4;
-        MemRead = 1'b1;
+        Address = 4;
+        MemRead = 1;
         #20;
-        MemRead = 1'b0;
+        MemRead = 0;
 
         // Finish simulation
         #20;
         $finish;
-	
-	end
+    
+    end
 
 endmodule
-
