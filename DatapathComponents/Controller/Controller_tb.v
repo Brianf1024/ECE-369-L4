a7b6db372983cd84b8f6e480f3589f4c13b3065a
Joseph Sterling Wallace Jr <jwallace0125@arizona.edu>
2024-10-10 11:09:41 -0700

    Add files via upload

---
+++ DatapathComponents/Controller/Controller_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/10/2024 11:05:34 AM
+// Design Name: 
+// Module Name: Controller_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Controller_tb(
+
+    );
+endmodule
